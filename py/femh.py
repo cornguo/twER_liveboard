3a166913e408163c74264a73970f9b42ec852d48
lanfon72 <lanfon72@gmail.com>
2014-08-10 19:25:40 +0800

    fix femh's am/pm recognition.

--- py/femh.py
+++ py/femh.py
@@ -6,6 +6,9 @@ os.environ['TZ'] = 'ROC'
 html = requests.get('http://www.femh.org.tw/research/news_op.aspx')
 
 update_time =re.findall(u'日期：(.*)</span>',html.text)
+update_time.append( datetime.now().strftime('%p') )
+update_time = ' '.join(update_time)
+
 full_reported = re.findall(u'滿床：.*(.+?)</span>',html.text)
 pending = re.findall(u'人數：.*(.[0-9])</span>',html.text)
 # parse [doctor, bed, ward, ICU] like ['>1', '38', '70', '>0']
@@ -15,7 +18,7 @@ keys = ['pending_doctor','pending_bed', 'pending_ward', 'pending_icu']
 report = { key:value for value, key in zip(values, keys) }
 report['Hosptial_sn'] = '1131010011'
 report['full_reported'] = True if full_reported[0] == u'是' else False
-report['update_time'] = datetime.strptime(update_time[0], '%Y-%m-%d %H:%M').strftime('%s')
+report['update_time'] = datetime.strptime(update_time, '%Y-%m-%d %I:%M %p').strftime('%s')
 
 print ( json.dumps(report, ensure_ascii=False) )
 # [{"update_time": "1407185520", "pending_ICU": 0, "pending_bed": 37, "full_reported": true, "Hosptial_SN": "1131010011", "pending_doctor": 4, "pending_ward": 56}]
